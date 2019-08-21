9fde671487a0674b3560bcfd2398d73058a76086
Shubhanshu Chauhan <shchauha@uaf-10.t2.ucsd.edu>
2019-08-21 12:31:09 -0700

    removing complied class files

--- analysis/yields/doAll2016.C
+++ analysis/yields/doAll2016.C
@@ -27,28 +27,28 @@
     ch_data.Add(basedir_data+"Data*.root");
     ScanChain(&ch_data, options, outputdir);
 
-    // // Data-Driven Fakes
-    TChain ch_fakes("t", "fakes");
-    ch_fakes.Add(basedir_data+"Data*.root");
-    ScanChain(&ch_fakes, options + "doFakes", outputdir);
+    // // // Data-Driven Fakes
+    // TChain ch_fakes("t", "fakes");
+    // ch_fakes.Add(basedir_data+"Data*.root");
+    // ScanChain(&ch_fakes, options + "doFakes", outputdir);
 
-    // Data-Driven Flips
-    TChain ch_flips("t", "flips");
-    ch_flips.Add(basedir_data+"Data*.root");
-    ScanChain(&ch_flips, options + "doFlips", outputdir);
+    // // Data-Driven Flips
+    // TChain ch_flips("t", "flips");
+    // ch_flips.Add(basedir_data+"Data*.root");
+    // ScanChain(&ch_flips, options + "doFlips", outputdir);
 
-    // Monte-Carlo Backgrounds
-    TChain ch_ttw("t", "ttw");
-    ch_ttw.Add(basedir_mc+"TTWnlo.root");
-    ScanChain(&ch_ttw, options, outputdir);
+    // // Monte-Carlo Backgrounds
+    // TChain ch_ttw("t", "ttw");
+    // ch_ttw.Add(basedir_mc+"TTWnlo.root");
+    // ScanChain(&ch_ttw, options, outputdir);
 
-    TChain ch_ttz("t", "ttz");
-    ch_ttz.Add(basedir_mc+"TTZnlo.root");
-    ScanChain(&ch_ttz, options, outputdir);
+    // TChain ch_ttz("t", "ttz");
+    // ch_ttz.Add(basedir_mc+"TTZnlo.root");
+    // ScanChain(&ch_ttz, options, outputdir);
 
-    TChain ch_tth("t", "tth");
-    ch_tth.Add(basedir_mc+"TTHtoNonBB.root");
-    ScanChain(&ch_tth, options, outputdir);
+    // TChain ch_tth("t", "tth");
+    // ch_tth.Add(basedir_mc+"TTHtoNonBB.root");
+    // ScanChain(&ch_tth, options, outputdir);
 
     // TChain ch_dy("t", "dy");
     // /* ch_dy.Add(basedir_mc+"DY_low.root"); */
@@ -70,43 +70,43 @@
     // ch_vv.Add(basedir_mc+"ZZ.root");
     // ScanChain(&ch_vv, options, outputdir);
 
-    TChain ch_wz("t", "wz");
-    ch_wz.Add(basedir_mc+"WZ.root");
-    ScanChain(&ch_wz, options, outputdir);
+    // TChain ch_wz("t", "wz");
+    // ch_wz.Add(basedir_mc+"WZ.root");
+    // ScanChain(&ch_wz, options, outputdir);
 
-    // TChain ch_ww("t", "ww");
-    // ch_ww.Add(basedir_mc+"WW.root");
-    // ScanChain(&ch_ww, options, outputdir);
+    // // TChain ch_ww("t", "ww");
+    // // ch_ww.Add(basedir_mc+"WW.root");
+    // // ScanChain(&ch_ww, options, outputdir);
 
-    TChain ch_rares("t", "rares");
-    ch_rares.Add(basedir_mc+"WZG.root");
-    ch_rares.Add(basedir_mc+"WZZ.root");
-    ch_rares.Add(basedir_mc+"ZZZ.root");
-    ch_rares.Add(basedir_mc+"WWZ.root");
-    ch_rares.Add(basedir_mc+"WWW.root");
-    ch_rares.Add(basedir_mc+"TZQ.root");
-    ch_rares.Add(basedir_mc+"TTZZ.root");
-    ch_rares.Add(basedir_mc+"TTWZ.root");
-    ch_rares.Add(basedir_mc+"TTWW.root");
-    ch_rares.Add(basedir_mc+"TTWH.root");
-    ch_rares.Add(basedir_mc+"TTHH.root");
-    ch_rares.Add(basedir_mc+"TTTW.root");
-    ch_rares.Add(basedir_mc+"TTTJ.root");
-    ScanChain(&ch_rares, options, outputdir);
+    // TChain ch_rares("t", "rares");
+    // ch_rares.Add(basedir_mc+"WZG.root");
+    // ch_rares.Add(basedir_mc+"WZZ.root");
+    // ch_rares.Add(basedir_mc+"ZZZ.root");
+    // ch_rares.Add(basedir_mc+"WWZ.root");
+    // ch_rares.Add(basedir_mc+"WWW.root");
+    // ch_rares.Add(basedir_mc+"TZQ.root");
+    // ch_rares.Add(basedir_mc+"TTZZ.root");
+    // ch_rares.Add(basedir_mc+"TTWZ.root");
+    // ch_rares.Add(basedir_mc+"TTWW.root");
+    // ch_rares.Add(basedir_mc+"TTWH.root");
+    // ch_rares.Add(basedir_mc+"TTHH.root");
+    // ch_rares.Add(basedir_mc+"TTTW.root");
+    // ch_rares.Add(basedir_mc+"TTTJ.root");
+    // ScanChain(&ch_rares, options, outputdir);
 
-    TChain ch_singletop("t", "singletop");
-    ch_singletop.Add(basedir_mc+"ST1.root");
-    ch_singletop.Add(basedir_mc+"ST2.root");
-    ScanChain(&ch_singletop, options, outputdir);
+    // TChain ch_singletop("t", "singletop");
+    // ch_singletop.Add(basedir_mc+"ST1.root");
+    // ch_singletop.Add(basedir_mc+"ST2.root");
+    // ScanChain(&ch_singletop, options, outputdir);
 
-    TChain ch_ttdl("t", "ttdl");
-    ch_ttdl.Add(basedir_mc+"TTDL.root");
-    ScanChain(&ch_ttdl, options, outputdir);
+    // TChain ch_ttdl("t", "ttdl");
+    // ch_ttdl.Add(basedir_mc+"TTDL.root");
+    // ScanChain(&ch_ttdl, options, outputdir);
 
-    TChain ch_fcnc("t", "fcnc");
-    ch_fcnc.Add("/nfs-7/userdata/namin/tupler_babies/merged/FT/v3.24/output/year_2016/FCNC_hut_top.root");
-    ch_fcnc.Add("/nfs-7/userdata/namin/tupler_babies/merged/FT/v3.24/output/year_2016/FCNC_hut_antitop.root");
-    ScanChain(&ch_fcnc, options, outputdir);
+    // TChain ch_fcnc("t", "fcnc");
+    // ch_fcnc.Add("/nfs-7/userdata/namin/tupler_babies/merged/FT/v3.24/output/year_2016/FCNC_hut_top.root");
+    // ch_fcnc.Add("/nfs-7/userdata/namin/tupler_babies/merged/FT/v3.24/output/year_2016/FCNC_hut_antitop.root");
+    // ScanChain(&ch_fcnc, options, outputdir);
 
 
 }
